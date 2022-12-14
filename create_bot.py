45cc2627cf0d707b26f526522eef3ed2741bd62a
posthak <adler_m@mail.ru>
2022-12-14 18:59:56 +0600

    Test

--- create_bot.py
+++ create_bot.py
@@ -3,8 +3,9 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-
+# 
 TOKEN = "5619440999:AAGVYf_v018LLUy21eUxcoPaNMuRuaf1JI0"
 bot  = Bot(token = TOKEN)
 dp = Dispatcher(bot,storage=storage)
+
 
